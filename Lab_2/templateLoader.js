5d784fa0f1fff66e80faee3fdd74cf9ad26c4710
GameTrue <sasfomin@gmail.com>
2024-10-04 21:51:41 +0300

    fizxes + doing for lab3

---
+++ Lab_2/templateLoader.js
@@ -0,0 +1,29 @@
+function loadFragment(fragmentId, url, callback) {
+	fetch(url)
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById(fragmentId).innerHTML = data;
+			if (callback) callback(); // Вызов callback после загрузки
+		})
+		.catch(error => console.error('Ошибка загрузки фрагмента:', error));
+}
+
+// Подгрузка header и footer
+loadFragment('header-placeholder', 'header.html', setActiveLink);
+loadFragment('footer-placeholder', 'footer.html');
+
+
+// Функция для установки класса active-link на активную страницу
+function setActiveLink() {
+	const path = window.location.pathname;
+	// Извлекаем последний элемент пути (название страницы)
+	const page = path.substring(path.lastIndexOf('/') + 1);
+	const menuItems = document.querySelectorAll('nav a');
+
+	menuItems.forEach(link => {
+		console.log(link.getAttribute('href'), path)
+		if (link.getAttribute('href') === page) {
+			link.querySelector('li').classList.add('active-link');
+		}
+	});
+}
